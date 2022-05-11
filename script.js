11cd73727ed90eea8fff1718dc01acae27ef0325
EdelDenis <edeldenis12@gmail.com>
2022-05-11 16:01:55 +0300

    l

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict"
+
+// -----------Навигация по DOM - элементам, data-атрибуты, преимущество for/of--------
+
+// console.log(document.body); //-- в консоли боди элемент 
+
+//console.log(document.documentElement); //- в консоле весь хтмл документ
+
+//console.log(document.body.childNodes); - в консоле узлы которые являются детьми у боди
